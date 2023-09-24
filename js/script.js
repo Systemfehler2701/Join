a99f4f2c1e5dcb8b226eea04fa26da5ad28e4ee5
Systemfehler2701 <Marnie.Lindenthal@web.de>
2023-09-24 20:08:53 +0200

    navigation und header überarbeitet

--- js/script.js
+++ js/script.js
@@ -1,13 +1,63 @@
-async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html");
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
-    }
+function app() {
+    renderLayout();
+    renderNavigation();
+    renderHeader();
+    renderSummary();
+}
+
+function renderLayout() {
+    let content = document.getElementById('content-app');
+    content.innerHTML = '';
+    content.innerHTML = /* html */ `
+    <div id="left-layout"></div>
+    <div id="right-layout">
+        <div id="header"></div>
+        <div id="content"></div>
+    </div>`;
+}
+
+function renderNavigation() {
+    let content = document.getElementById('left-layout');
+    content.innerHTML = /* html */ `
+    <nav>
+        <div class = "navigation-img" >
+            <img class = "navigation-logo"src = "/assets/img/logo-white.svg">
+        </div>
+        <div class = "navigation-menu">
+            <div class = "menu-button" onclick = "navigationMenuClicked(this)">
+                <div class = "menu-icon summary"></div><span> Summary </span>
+            </div>
+            <div class = "menu-button" onclick = "navigationMenuClicked(this)">
+                <div class = "menu-icon add-task" ></div><span>Add Task</span>
+            </div>
+            <div class = "menu-button" onclick = "navigationMenuClicked(this)">
+                <div class = "menu-icon board" ></div><span>Board</span>
+            </div>
+            <div class = "menu-button" onclick = "navigationMenuClicked(this)">
+                <div class = "menu-icon contacts"></div><span>Contacts</span>
+            </div>
+        </div>
+        <div class = "footer">
+            <a href ="">Privacy Policiy </a>
+            <a href ="">Legal notice</a>
+        </div >
+    </nav>`;
+}
+
+function renderHeader() {
+    let content = document.getElementById('header');
+    content.innerHTML = /* html */ `
+        <div class="headline">
+            <span>Kanban Project Management Tool</span>
+        </div>
+        <div class="headline-profile">
+            <div class="help-link">
+                <div onclick="renderHelp()"><img src="/assets/img/help.svg" ></div>
+            </div>
+            <div onclick="openSubmenu()" class="user-profile-initials"></div>
+        </div>`;
+}
+
+function renderHelp() {
+
 }
---
+++ js/summary.js
@@ -0,0 +1,10 @@
+function renderSummary() {
+    let content = document.getElementById('content');
+    content.innerHTML = '';
+    content.innerHTML = /* html */ `
+    <div class="header-content">
+        <h1>Join 360</h1>
+        <img src="/assets/img/blue-stroke.svg">
+        <h2>Key Matrics at a Glance</h2>
+    </div>`;
+}
