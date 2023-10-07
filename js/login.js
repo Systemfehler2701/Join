f19a6874a39ac1396f729f18e0b5dabac88921e6
Systemfehler2701 <Marnie.Lindenthal@web.de>
2023-10-07 10:02:00 +0200

    login renderdatei hinzugefügt

--- js/login.js
+++ js/login.js
@@ -1,23 +1,6 @@
 let currentUser = null;
 let isGuestUser = false;
 
-function renderLoginMask() {
-    var container = document.getElementById("content-app");
-
-    container.innerHTML = `<div id="login_signupBody">
-    <div class="login-headline">
-      <img id="logo" src="../../assets/img/logo_main.svg" alt="Logo">
-    </div>
-      <form onsubmit="logIn(); return false;" id="login_form">
-        <h1>Log in</h1>
-        <input type="email" id="loginEmail" placeholder="Email" />
-        <input type="password" id="loginPassword" placeholder="Password" />
-        <button type="submit">Log in</button>
-        <button type="button" onclick="logInGuest()">Guest Log in</button>
-      </form>
-      <a onclick=renderHelp()></a>
-      </div>`;
-}
 
 async function logIn() {
     try {
