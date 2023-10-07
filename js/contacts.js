22ca4d1dd612f37590996bd1196cc10738fd5a42
Systemfehler2701 <Marnie.Lindenthal@web.de>
2023-10-07 11:24:59 +0200

    kleiner fehler

--- js/contacts.js
+++ js/contacts.js
@@ -29,7 +29,7 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 const colors = ['#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8', '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701', '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B'];
 
 async function renderContactList() {
-    users = await getItem('contacts');
+    users = JSON.parse(await getItem('contacts'));
     let content = '';
     let currentInitial = '';
 
