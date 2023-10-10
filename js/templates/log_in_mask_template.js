4efbefe4bc662626ae3a68fb75d11101dca14f45
Systemfehler2701 <Marnie.Lindenthal@web.de>
2023-10-10 21:21:04 +0200

    fadeIn logo

--- js/templates/log_in_mask_template.js
+++ js/templates/log_in_mask_template.js
@@ -1,8 +1,8 @@
 function renderLoginMask() {
-  var container = document.getElementById("content-app");
-  var rememberedEmail = localStorage.getItem("rememberedEmail");
+    var container = document.getElementById("content-app");
+    var rememberedEmail = localStorage.getItem("rememberedEmail");
 
-  container.innerHTML = /*html*/ `<div id="login_signupBody">
+    container.innerHTML = /*html*/ `<div id="login_signupBody">
     <div class="login-headline">
       <img id="logo" src="../../assets/img/logo_main.svg" alt="Logo">
       <div class="headline-right">
@@ -10,30 +10,32 @@ function renderLoginMask() {
         <button id="SignUpbtn" type="button" onclick="renderSignUpMask()">Sign Up</button>
       </div>
     </div>
+    <div class="login-area">
       <form onsubmit="logIn(); return false;" id="login_form">
         <div id="loginHead">
-        <h1 id="loginTitle">Log in</h1>
-        <div id="loginInput">
-        <input type="email" id="loginEmail" placeholder="Email" />
-        <input type="password" id="loginPassword" placeholder="Password" />
-        </div>
-        <div id="loginRememberMe">
-        <input type="checkbox" id="loginRemember" />
-        <span>Remember me</span>
-        </div>
-        <div id="loginButtons">
-        <button type="submit">Log in</button>
-        <button class="guest-login-btn" type="button" onclick="logInGuest()">Guest Log in</button>
-      </div>
+          <h1 id="loginTitle">Log in</h1>
+          <div id="loginInput">
+            <input type="email" id="loginEmail" placeholder="Email" />
+            <input type="password" id="loginPassword" placeholder="Password" />
+          </div>
+          <div id="loginRememberMe">
+            <input type="checkbox" id="loginRemember" />
+            <span>Remember me</span>
+          </div>
+          <div id="loginButtons">
+            <button type="submit">Log in</button>
+            <button class="guest-login-btn" type="button" onclick="logInGuest()">Guest Log in</button>
+          </div>
+        </div>  
       </form>
-      </div>
+    </div>
       <div id="legalNotice">
       <a href="privacy_policy.html" target="_blank">Privacy Policy</a>
       <a href="legal_notice.html" target="_blank">Legal Notice</a>
       </div>`;
-      
-      if (rememberedEmail) {
+
+    if (rememberedEmail) {
         // E-Mail-Adresse in das Eingabefeld einfügen
         document.getElementById("loginEmail").value = rememberedEmail;
-      }
+    }
 }
