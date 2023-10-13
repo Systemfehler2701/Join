09f6af6df7ac8e28a4e0bdc28672fed65ea317a6
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2023-10-13 19:29:19 +0200

    overlay

--- js/templates/contacts_template.js
+++ js/templates/contacts_template.js
@@ -5,36 +5,40 @@ function renderContacts(element) {
     content.innerHTML = /* html */ `
     <div id="leftside" class="contact-leftside">
         <div class="contact-left-button">
-        <button class="addButton">
-            <span>Add new Contact</span>
-            <img src="assets/img/person_add.svg">
-        </button>
+            <button class="addButton" onclick="openContactOverlay()">
+                <span>Add new Contact</span>
+                <img src="assets/img/person_add.svg">
+            </button>
         </div>
         <div class="viewall scroll">
             <button id="responsiveButton"><img src="assets/img/person_add.svg"></button>
             <div style="width: 100%" id="contactlist"></div>
         </div>
     </div>
-
-        <div class="rightside" id="contactsforRespons">
+    <div class="rightside" id="contactsforRespons">
         <div class="arrowBack">
-        <img src="/assets/img/arrow-left-line.svg" alt="Back" id="backArrow" style="display:none;">
-</div>
-            <div class="contacts-headline">
-                <h1>Contacts</h1>
-                  <img class="contact-img" src="/assets/img/blue-stroke.svg">
-                  <h2>Better with a team</h2>
-                  <img class="contact-img-responsive" src="/assets/img/Vector 5.svg">
-            </div>
-            <div class="contactInfo" id="detailsContainer"></div>
-            <div class="responsiveMenu">
+            <img src="/assets/img/arrow-left-line.svg" alt="Back" id="backArrow" style="display:none;">
+        </div>
+        <div class="contacts-headline">
+            <h1>Contacts</h1>
+            <img class="contact-img" src="/assets/img/blue-stroke.svg">
+            <h2>Better with a team</h2>
+            <img class="contact-img-responsive" src="/assets/img/Vector 5.svg">
+        </div>
+        <div class="contactInfo" id="detailsContainer"></div>
+        <div class="responsiveMenu">
             <button id="menu"><img src="assets/img/more_vert.svg"></button>
+        </div>
+        <div id="contactOverlay" class="overlay-hidden">
+            <div class="iconWrapper" onclick="renderEditContact()">
+                <img class="icon" src="/assets/img/edit.svg">
+                <span class="iconText">Edit</span>
             </div>
- 
-            <div id="contactOverlay" class="overlay-hidden">
-    <div class="iconWrapper" onclick="renderEditContact()">
-        <img class="icon" src="/assets/img/edit.svg">
-        <span class="iconText">Edit</span>
+            <div class="iconWrapper" onclick="deleteContact(currentlyDisplayedContactIndex)">
+                <img class="icon" src="/assets/img/delete.svg">
+                <span class="iconText">Delete</span>
+            </div>
+        </div>
     </div>
     <!-- Overlay neu -->
 <div class="contacts-overlay-content" id="overlay"> 
@@ -48,6 +52,7 @@ function renderContacts(element) {
             </div>
         </div>
         <div class="whiteside">
+        <button class="goBack"><img src="assets/img/close.svg"></button>
             <div class="contacts-overlay-whiteside-left">
                 <div class="contacts-overlay-icon">
                     <img src="../assets/img/person2.svg">
@@ -57,12 +62,12 @@ function renderContacts(element) {
                 <div class="contacts-overlay-close"><img src=""></div>
                 <div class="inputContainer">
                     <form class="inputArea" onsubmit="">
-                        <input class="inputName" type="text" id="editName" required>  
-                        <input class="inputMail" type="email" id="editEmail" required>           
-                        <input class="inputPhone" type="tel" id="editPhone" required>
+                        <input class="inputName" type="text"placeholder="Surname Name" id="editName" required>  
+                        <input class="inputMail" type="email"placeholder="Mail" id="editEmail" required>           
+                        <input class="inputPhone" type="tel"placeholder="Phone" id="editPhone" required>
                         <div id="contacts-overlay-buttons">
-                            <button></button>
-                            <button></button>
+                            <button class="cancelBtn">Cancel<img src="assets/img/close.svg"></button>
+                            <button class="createBtn">Create Contact</button>
                         </div>    
                     </form>  
                 </div>
@@ -70,38 +75,6 @@ function renderContacts(element) {
         </div>    
     </div>
 </div>
-    </div>
-
-    <  <!-- Overlay -->
-
-    <div class="overlay">
-            <div class="containerAll">
-                <div class="darkside">
-                    <div class="image-to-hide">
-                        <span class="blueLineHorizontal"></span>
-                    </div>
-                </div>
-                        <div class="whiteside">
-                                
-                                    
-                        </div>    
-                            <div class="inputContainer">
-                                <form class="inputArea">
-                                    <input type="hidden">
-                                                <input class="inputName" type="text" id="editName" required>  
-                                                <input class="inputMail" type="email" id="editEmail" required>           
-                                                <input class="inputPhone" type="tel" id="editPhone" required>
-                                        <div class="buttonArea">
-                                            <button></button>
-                                            <button></button>
-                                            <button></button>
-                                </form>  
-                                        </div>
-                            </div>
-                        
-            </div>    
-    </div>
-
     `;
-    addContactsEventlistener();
+    renderContactList();
 }
